ac35dd6fb8ee1dd867a088d4e43f63dfba812676
Feyza Rafiq <feyza.rafiq@wellola.com>
2022-03-21 16:10:28 +0000

    quest 2 secu formulaire

--- form_quest2/form_quest2.php
+++ form_quest2/form_quest2.php
@@ -1,36 +1,102 @@
-  <form action="thanks2.php" method="post">
+<?php
+// define variables and set to empty values
+$lastnameErr = $firstnameErr = $emailErr = $phoneErr = $objectErr = $messageErr = "";
+$lastname = $firstname = $email = $phone = $object = $message = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["lastname"])) {
+    $lastnameErr = "Lastname is required";
+  } else {
+    $lastname = test_input($_POST["lastname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $lastname)) {
+      $lastnameErr = "Only letters and white space allowed";
+    }
+  }
 
-    <div>
-      <label for="lastname">Lastname :</label>
-      <input type="text" id="lastname" name="user_lastname" placeholder="lastname">
-    </div>
-    <div>
-      <label for="firstname">Firstname :</label>
-      <input type="text" id="firstname" name="user_firstname" placeholder="firstname">
-    </div>
-    <div>
-      <label for="courriel">Courriel :</label>
-      <input type="email" id="courriel" name="user_email" placeholder="xxx@email.com">
-    </div>
-    <div>
-      <label for="phone">Phone number :</label>
-      <input type="tel" id="phone" name="user_phone" pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" placeholder="06 11 22 33 44">
-    </div>
-    <div>
-      <label for="message">Message :</label>
-      <textarea id="message" name="user_message" placeholder="Your message"></textarea>
-    </div>
-    <div>
-      <label for="object">Object :</label>
-      <select id="object" name="user_object">
-        <option value="">Please select an object</option>
-        <option value="enquirements">Enquirements</option>
-        <option value="refunds">Refunds</option>
-        <option value="other">Other</option>
-      </select>
-    </div>
-    <div class="button">
-      <button type="submit">Envoyer votre message</button>
-    </div>
-  </form>
+  if (empty($_POST["firstname"])) {
+    $firstnameErr = "Firstname is required";
+  } else {
+    $firstname = test_input($_POST["firstname"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $firstname)) {
+      $firstnameErr = "Only letters and white space allowed";
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+  if (empty($_POST["phone"])) {
+    $phoneErr = "Phone is required";
+  } else {
+    $phone = test_input($_POST["phone"]);
+  }
+
+  if (empty($_POST["message"])) {
+    $messageErr = "Message is required";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
+
+  if (empty($_POST["object"])) {
+    $objectErr = "Object is required";
+  } else {
+    $object = test_input($_POST["object"]);
+  }
+}
+
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+  <div>
+    <label for="lastname">Lastname :</label>
+    <input type="text" id="lastname" name="lastname" placeholder="lastname">
+    <span><?php echo $lastnameErr; ?></span>
+  </div>
+  <div>
+    <label for="firstname">Firstname :</label>
+    <input type="text" id="firstname" name="firstname" placeholder="firstname">
+    <span><?php echo $firstnameErr; ?></span>
+  </div>
+  <div>
+    <label for="courriel">Courriel :</label>
+    <input type="email" id="email" name="email" placeholder="xxx@email.com">
+    <span><?php echo $emailErr; ?></span>
+  </div>
+  <div>
+    <label for="phone">Phone number :</label>
+    <input type="tel" id="phone" name="phone" pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" placeholder="06 11 22 33 44">
+    <span><?php echo $phoneErr; ?></span>
+  </div>
+  <div>
+    <label for="message">Message :</label>
+    <textarea id="message" name="message" placeholder="Your message"></textarea>
+    <span><?php echo $messageErr; ?></span>
+  </div>
+  <div>
+    <label for="object">Object :</label>
+    <select id="object" name="object">
+      <option value="">Please select an object</option>
+      <option value="enquirements">Enquirements</option>
+      <option value="refunds">Refunds</option>
+      <option value="other">Other</option>
+    </select>
+    <span><?php echo $objectErr; ?></span>
+  </div>
+  <div class="button">
+    <button type="submit">Envoyer votre message</button>
+  </div>
+</form>
--- form_quest2/thanks2.php
+++ form_quest2/thanks2.php
@@ -1,12 +1,12 @@
 <?php
-// echo  $_POST['user_lastname'];
-// echo  $_POST['user_firstname'];
-// echo  $_POST['user_email'];
-// echo  $_POST['user_phone'];
-// echo  $_POST['user_message'];
-// echo  $_POST['user_object'];
+// echo  $_POST['lastname'];
+// echo  $_POST['firstname'];
+// echo  $_POST['email'];
+// echo  $_POST['phone'];
+// echo  $_POST['message'];
+// echo  $_POST['object'];
 
-echo "Merci {$_POST['user_firstname']} {$_POST['user_lastname']} de nous avoir contacté à propos de {$_POST['user_object']}. <br>
-Un de nos conseillers vous contactera soit à l’adresse {$_POST['user_email']} ou par téléphone au {$_POST['user_phone']} dans les plus brefs délais pour traiter votre demande : {$_POST['user_message']}";
+echo "Merci {$_POST['firstname']} {$_POST['lastname']} de nous avoir contacté à propos de {$_POST['object']}. <br>
+Un de nos conseillers vous contactera soit à l’adresse {$_POST['email']} ou par téléphone au {$_POST['phone']} dans les plus brefs délais pour traiter votre demande : {$_POST['message']}";
 
 ?>
